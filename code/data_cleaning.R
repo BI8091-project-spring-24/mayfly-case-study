297335126f93d5b1d8dd729c08615ac5a58f5a79
beatrimt <beatrice.m.trascau@ntnu.no>
2024-01-17 11:38:30 +0100

    Remove records flagged as temporal outliers

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -42,20 +42,51 @@ ggplot() +
              size = 0.5) +
   theme_classic()
 
-## 2.2. Remove records with erronous coorindates ----
+## 2.2. Remove records with problematic coordinates ----
 
 # Extract coordinate flags
-coordinate_flags <- clean_coordinates(x = insectdata,
+coord_flags <- clean_coordinates(x = insectdata,
                                       lon = "decimalLongitude", 
                                       lat = "decimalLatitude",
                                       species = "acceptedScientificName",
                                       test = c("centroids", "equal", "gbif", "zeros"))
 
 # Get a summary of the detected flags
-summary(coordinate_flags) # only 3 records flagged (centroids)
+summary(coord_flags) # only 3 records flagged (centroids)
 
 # Plot flagged records
-plot(coordinate_flags, lon = "decimalLongitude", lat = "decimalLatitude")
+plot(coord_flags, lon = "decimalLongitude", lat = "decimalLatitude")
 
 # Exclude flagged records
-insectdata_no_flags <- insectdata[coordinate_flags$.summary, ]
+insectdata_coords <- insectdata[coord_flags$.summary, ]
+
+## 2.3. Remove records with temporal outliers ----
+
+# Test for temporal outliers on taxon level
+time_flags <- cf_age(x = insectdata_no_flags,
+                  lon = "decimalLongitude",
+                  lat = "decimalLatitude",
+                  taxon = "species", 
+                  min_age = "year", 
+                  max_age = "year", 
+                  value = "flagged") # Flagged 8 records
+
+# Exclude records flagged for temporal outliers
+insectdata_no_flags <- insectdata_coords[time_flags, ]  
+
+# 3. IMPROVE DATA QUALITY WITH META-DATA ----
+
+## 3.1. Coordinate precision ----
+
+
+
+
+
+
+
+
+
+
+
+
+
