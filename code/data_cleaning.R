38f385d334204e769a02fe9184508fa54b143b9c
beatrimt <beatrice.m.trascau@ntnu.no>
2024-01-17 11:12:59 +0100

    Inspect data, extract & plot flagged records

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -6,6 +6,7 @@
 
 # 0. PACKAGES ----
 library(here)
+library(ggplot2)
 library(CoordinateCleaner)
 library(dplyr)
 library(terra)
@@ -24,3 +25,34 @@ download.file(mayfly_records, here("data", "insectdata.rda"))
 ## 1.2. Load data ----
 load(here("data", "insectdata.rda"))
 
+# 2. CLEAN GBIF RECORDS ----
+
+## 2.1. Inspect data ----
+
+# Download world map 
+wm <- borders("world", colour = "gray50", fill = "gray50")
+
+# Plot data to get an overview
+ggplot() +
+  coord_fixed() +
+  wm +
+  geom_point(data = insectdata,
+             aes(x = decimalLongitude, y = decimalLatitude),
+             colour = "darkred",
+             size = 0.5) +
+  theme_classic()
+
+## 2.2. Remove records with erronous coorindates ----
+
+# Extract coordinate flags
+coordinate_flags <- clean_coordinates(x = insectdata,
+                                      lon = "decimalLongitude", 
+                                      lat = "decimalLatitude",
+                                      species = "acceptedScientificName",
+                                      test = c("centroids", "equal", "gbif", "zeros"))
+
+# Get a summary of the detected flags
+summary(coordinate_flags)
+
+# Plot flagged records
+plot(coordinate_flags, lon = "decimalLongitude", lat = "decimalLatitude")
