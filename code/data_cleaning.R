a2720c98e88aadf7573482b5312ba522cbf4cb6d
beatrimt <beatrice.m.trascau@ntnu.no>
2024-01-17 11:03:33 +0100

    Load in data

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -7,14 +7,20 @@
 # 0. PACKAGES ----
 library(here)
 library(CoordinateCleaner)
+library(dplyr)
 library(terra)
 library(sf)
-library(dplyr)
 
-# 1.DOWNLOAD RECORDS FROM BOX ----
+# 1. LOAD DATA ----
+
+## 1.1. Download records from Box ----
 
 # Add downaload link ----
 mayfly_records <- ("https://ntnu.box.com/shared/static/oky8o2cha6nek1jjexum29qqh0fk7asm.rda")
 
 # Download file
 download.file(mayfly_records, here("data", "insectdata.rda"))
+
+## 1.2. Load data ----
+load(here("data", "insectdata.rda"))
+
