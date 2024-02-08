cc34a9b729c859c069a7715652861ec4b23c3aae
beatrimt <beatrice.m.trascau@ntnu.no>
2024-02-08 10:59:14 +0100

    Creating mesh object from occurrence records - WIP

--- code/integrated_SDM.R
+++ code/integrated_SDM.R
@@ -10,8 +10,11 @@ library(dplyr)
 library(terra)
 library(sf)
 library(PointedSDMs)
+library(INLA)
 
-# 1. RUN POINTEDSDMS ----
+# 1. PREPARE DATA FOR POINTEDSDMS ----
+
+## 1.1. Load data ----
 
 # Set options for the INLA package to use it "experimental" features
 bru_options_set(inla.mode = "experimental")
@@ -23,12 +26,22 @@ load(here("data", "cleaned_insectdata.rda"))
 projection <- "+proj=longlat +ellps=WGS84"
 
 # Extract species occurrence records
-species <- SolitaryTinamou$datasets
+insect_data <- cleaned_insectdata
+
+# Read in climate data
+bio1 <- terra::rast(here("data", "bio1_norway.tif"))
 
 # Normalize climate data by scaling it
-# here they use NPP but we should use one of the climate variables
-NPP <- scale(terra::rast(system.file('extdata/SolitaryTinamouCovariates.tif', 
-                                     package = "PointedSDMs"))$NPP)
+bio1_scaled <- scale(bio1)
+
+## 1.2. Create mesh object ---- 
+# Mesh objects are used by INLA to aproximate spatial model for the IntegratedSDM
+
+# Create spatial points dataframe from occurrence records
+mayfly_points <- st_as_sf(insect_data, coords = c("decimalLongitude", "decimalLatitude"),
+                          crs = 4326)
+
+# Define a study area as a bounding box
 
 # Retrieve mesh object - not sure what a mesh object is an how to fix that
 mesh <- SolitaryTinamou$mesh
