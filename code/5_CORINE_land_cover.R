5fe5850e9df20bdb7da417718d1281bdbe51a708
Beatrice Maria Trascau <beatrimt@vm-srv-franklin.win.ntnu.no>
2024-04-15 14:33:36 +0200

    Change layer values and save the changed layer

--- code/5_CORINE_land_cover.R
+++ code/5_CORINE_land_cover.R
@@ -45,3 +45,63 @@ crs(norway_corine_projection, proj = TRUE) #"+proj=laea +lat_0=52 +lon_0=10 +x_0
 ## 2.3. Crop and mask CORINE 2018 to Norway ----
 norway_corine_2018 <- crop(corine_2018, norway_corine_projection,
                             mask = TRUE)
+
+## 2.4. Save cropped layer ----
+terra::writeRaster(norway_corine_2018,
+                   here("data", "norway_corine_2018.tif"),
+                   overwrite = TRUE)
+
+# 3. CHANGE COVER LAYER VALUES ----
+
+# The CORINE 2018 Land Cover Status Layer values will be aggregated to only maintain the values relevant for the analysis
+
+# Urban Fabric
+  # all the urban classes are pooled together, due to their sparse distribution across Norway
+norway_corine_modified <- app(norway_corine_2018,
+                              fun = function(x){x[x %in% c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)] <- 1; 
+                              return(x)})
+
+# Complex agricultural patterns
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x %in% c(12, 18, 20)] <- 20; 
+                              return(x)})
+
+#Agriculture and significant natural vegetation
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x == 21] <- 21; 
+                              return(x)})
+
+#Forests
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x %in% c(23, 24, 25)] <- 25; 
+                              return(x)})
+
+#Moors, Heathland & Natural Grassland
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x %in% c(26, 27)] <- 26; 
+                              return(x)})
+#Transitional woodland shrub
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x == 29] <- 29; return(x)})
+
+#Sparsely vegetated areas
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x == 32] <- 32; return(x)})
+
+# Water courses
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x == 40] <- 40; return(x)})
+
+# Water bodies
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x == 41] <- 41; return(x)})
+
+#Other classes
+norway_corine_modified <- app(norway_corine_modified,
+                              fun = function(x){x[x %in% c(30, 31, 33, 34, 35, 36, 39, 43, 44, 128)] <- 0; 
+                              return(x)})
+
+# Save the modified corine stack 
+terra::writeRaster(norway_corine_modified,
+                   here("data", "corine_2018_modified_classes.tif"),
+                   overwrite = TRUE)
