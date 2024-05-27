50e8d4b65aed1906ff0a854d5651297a601e9599
Hanne B Krogstie <hanne.krogstie@hotmail.com>
2024-05-27 09:44:51 +0200

    Added header and documentation to code.

--- code/0_setup.R
+++ code/0_setup.R
@@ -1,3 +1,10 @@
+################################################################################
+
+# 0. Setup
+
+################################################################################
+
+# Install packages function
 install.load.package <- function(x) {
   if (!require(x, character.only = TRUE)) {
     install.packages(x, repos = "http://cran.us.r-project.org")
@@ -5,20 +12,21 @@ install.load.package <- function(x) {
   require(x, character.only = TRUE)
 }
 
+# Vector specifying which packages to download/load
 package_vec <- c("here","dplyr","knitr",
                  "ggplot2","rgbif",
                  "here", "CoordinateCleaner",
                  "terra", "sf","mapview",
                  "PointedSDMs","stringr",
-                 "stars", "geodata") # specify packages
+                 "stars", "geodata")
 
-## executing install & load for each package
+## Executing install & load for each package
 sapply(package_vec, install.load.package)
 
-# INLA is not on CRAN, so has to be installed separatly
+
+# INLA is not on CRAN, so has to be installed separately
 #install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE) 
 library("INLA")
-
 
 conditional_download <- function(url, target) {
   if (!file.exists(target)) {
