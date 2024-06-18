eb38461bdc3f23d259bffb877615ab22f1357783
beatrimt <beatrice.m.trascau@ntnu.no>
2024-06-18 15:46:50 +0200

    Added tidyterra to package list

--- code/0_setup.R
+++ code/0_setup.R
@@ -19,7 +19,8 @@ package_vec <- c("here","dplyr","knitr",
                  "terra", "sf","mapview",
                  "PointedSDMs","stringr",
                  "stars", "geodata", 
-                 "ggspatial", "cowplot")
+                 "ggspatial", "cowplot",
+                 "tidyterra")
 
 ## Executing install & load for each package
 sapply(package_vec, install.load.package)
