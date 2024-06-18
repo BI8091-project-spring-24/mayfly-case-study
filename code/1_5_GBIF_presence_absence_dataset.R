a37c06914bdbd8461282d44ffa1a69f844e12b5d
beatrimt <beatrice.m.trascau@ntnu.no>
2024-06-18 14:32:12 +0200

    Removed records with problematic coordinates and created maps of the presences and absences

--- code/1_5_GBIF_presence_absence_dataset.R
+++ code/1_5_GBIF_presence_absence_dataset.R
@@ -37,13 +37,80 @@ events_NTNU <- occurrences_NTNU %>%
   group_by(eventID) %>%
   dplyr::summarise(scientificNames = paste0(unique(scientificName), collapse = ", "),
                    decimalLatitude = paste0(unique(decimalLatitude), collapse = ", "),
-                   decimalLongitude = paste0(unique(decimalLongitude), collapse = ", ")) 
-  
+                   decimalLongitude = paste0(unique(decimalLongitude), collapse = ", "),
+                   samplingProtocol = paste0(unique(samplingProtocol), collapse = ", ")) 
+
+# Inspect Coordinates
+sorted_latitudes <- sort(events_NTNU$decimalLatitude, decreasing = FALSE) #44.45 latitude is not in Norway
+sorted_longitudes <- sort(events_NTNU$decimalLongitude, decreasing = FALSE) #"-8.45843", "-8.4622", "-8.45942" not in Norway
+
+# Add presence absence column and remove records with faulty coordinates
 events_NTNU <- events_NTNU %>%
-  mutate(presence = dplyr::if_else(stringr::str_detect(scientificNames, regex("Baetis rhodani")),1,0))
-# 22444 observations (unique events)
+  mutate(presence = dplyr::if_else(stringr::str_detect(scientificNames, regex("Baetis rhodani")),1,0)) |>
+  # remove records with faulty coordinates
+  filter(!decimalLongitude %in% c("-8.45843", "-8.4622", "-8.45942")) |>
+  filter(!decimalLatitude == "44.45")
+# 22403 observations (unique events)
 
 # Save file
 save(events_NTNU,file = here::here("data","derived_data","presence_absence_dataset.Rda"))
 
+# Map of presences and absences ----
 
+# Download map of Norway
+norway <- geodata::gadm(country = "NOR", level = 0, 
+                        path = tempdir(),
+                        version = "latest")
+
+# Convert norway to sf object
+norway_sf <- st_as_sf(norway)
+
+# Convert occurrences to spatial dataframe for plotting
+events_NTNU_sf <-st_as_sf(events_NTNU,
+                         coords = c("decimalLongitude","decimalLatitude"),
+                         crs = crs(norway_sf))
+
+# Map for presences
+presences <- events_NTNU_sf |>
+  filter(presence == 1) |>
+  ggplot() +
+  geom_sf(data = norway_sf, fill = "lightgray", color = "white") +
+  geom_sf(aes(color = samplingProtocol)) +
+  annotation_north_arrow(location = "br", which_north = "true",
+                         pad_y = unit(0.8, "cm"),
+                         style = north_arrow_fancy_orienteering) +
+  annotation_scale(location = "br", width_hint = 0.35) +
+  theme_classic() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.position = "none")
+
+# Map for absences
+absences <- events_NTNU_sf |>
+  filter(presence == 0) |>
+  ggplot() +
+  geom_sf(data = norway_sf, fill = "lightgray", color = "white") +
+  geom_sf( aes(color = samplingProtocol)) +
+  theme_classic() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.position = "none",
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.line.y = element_blank())
+
+# Arrange the two plots in a single figure
+presence_absence_map_no_legend <- plot_grid(presences, absences,
+                                  ncol = 2, align = "hv", rel_heights = c(1, 1))
+
+# Extract legend from one of the plots
+legend <- get_legend(absences + theme(legend.position = "bottom"))
+
+# Arrange the two plots in a single figure
+presence_absence_map <- plot_grid(presence_absence_map_no_legend, legend, 
+                                  ncol = 1, rel_heights = c(1, 0.1))
+
+# Save to file
+ggsave(here("figures", "presence_absence_map.png"),
+       width=13, height=9)
